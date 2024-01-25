0c4aaf8252339ad94453b90b544254ffc0f2b6ce
talhabutt7 <talhabuttt@gmail.com>
2024-01-25 15:46:40 +0000

    If the user is signed in then show the homepage, otherwise root route to session new of devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'homepage/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  devise_for :users
   # Set Devise as the root route
-  root to: "devise/sessions#new"
+  devise_scope :user do
+    authenticated :user do
+      root to: "homepage#index", as: :authenticated_root
+    end
 
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  get 'homepage/index'
   # Set Homepage index as the root route
   # root "homepage#index"
 end
