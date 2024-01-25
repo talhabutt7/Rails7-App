0b5971068b6385371d3b20dd38d89120d70ac30f
talhabutt7 <talhabuttt@gmail.com>
2024-01-25 15:02:59 +0000

    Set Devise as the root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "homepage#index"
+  # Set Devise as the root route
+  root to: "devise/sessions#new"
+
+  # Set Homepage index as the root route
+  # root "homepage#index"
 end
