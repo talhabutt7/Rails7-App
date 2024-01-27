be0afab1c97096992a47b0b46cb23166781167bf
talhabutt7 <talhabuttt@gmail.com>
2024-01-27 01:09:06 +0000

    Custom route path after successful devise login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || homepage_index_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name, :email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:full_name, :email])
