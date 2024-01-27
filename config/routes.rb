67e5f1f63a681cfb34b067e5c90dafc564cf83bd
talhabutt7 <talhabuttt@gmail.com>
2024-01-27 01:09:31 +0000

    Devise authentication route commented

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,17 @@ Rails.application.routes.draw do
 
   devise_for :users
   # Set Devise as the root route
-  devise_scope :user do
-    authenticated :user do
-      root to: "homepage#index", as: :authenticated_root
-    end
-
-    unauthenticated do
-      root 'devise/sessions#new', as: :unauthenticated_root
-    end
-  end
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root to: "homepage#index", as: :authenticated_root
+  #   end
+  #
+  #   unauthenticated do
+  #     root 'devise/sessions#new', as: :unauthenticated_root
+  #   end
+  # end
 
   get 'homepage/index'
   # Set Homepage index as the root route
-  # root "homepage#index"
+  root "homepage#index"
 end
